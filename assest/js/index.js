265735c6554404d10e6a341c7d67fad662725615
SkyRedFx <sicksolid@hotmail.com>
2020-06-16 14:59:45 -0500

    Design added

---
+++ assest/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(window).scroll(function(){
+        var winTop = $(window).scrollTop();
+        if(winTop > 30){
+            $(".main-nav").addClass("navbar-color")
+        } else {
+            $(".main-nav").removeClass("navbar-color")
+        }
+    })
+});
